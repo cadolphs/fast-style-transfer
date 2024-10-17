f59be2d9bf99fd6315ca43eb6bed984fa88720d2
cadolphs <clemens.adolphs@1qbit.com>
2024-10-17 14:48:07 -0700

    Most initial django and testing setup

---
+++ src/functional_tests.py
@@ -0,0 +1,11 @@
+from seleniumbase import BaseCase
+import pytest
+
+
+class TestWebsite(BaseCase):
+    @pytest.fixture(autouse=True)
+    def inject_fixtures(self, live_server):
+        self.live_server = live_server
+
+    def test_basic(self):
+        self.open(self.live_server.url)
