07c853faecd7d0632520a05b6bd274bb33ef9548
cadolphs <clemens.adolphs@1qbit.com>
2024-10-22 19:50:42 -0700

    Put homepage into template

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,6 +1,12 @@
 # Create your tests here.
+from pytest_django.asserts import assertTemplateUsed
 
 
 def test_home_page_has_title(client):
     response = client.get("/")
     assert "Fast Style" in str(response.content)
+
+
+def test_home_page_uses_correct_template(client):
+    response = client.get("/")
+    assertTemplateUsed(response, "home.html")
--- src/functional_tests.py
+++ src/functional_tests.py
@@ -8,5 +8,22 @@ class TestWebsite(BaseCase):
         self.live_server = live_server
 
     def test_basic(self):
+        self.set_time_limit(1)
+        # Our awesome user, Alice, opens up her web browser
         self.open(self.live_server.url)
+
+        # She notices the title of the page is "Fast Style"
         self.assert_title("Fast Style")
+
+        return
+        # She sees a drag-and-drop area on the page that says "Drop an image here"
+        self.assert_element("div#drop-area")
+
+        # In the code, she sees that there's an input field
+        file_input = self.find_element('input[type="file"]')
+
+        # She manually sets the value of the input field to a test image
+        file_input.send_keys("test_image.jpg")
+
+        # She sees a thumbnail of the image she uploaded
+        self.assert_element("img#thumbnail")
